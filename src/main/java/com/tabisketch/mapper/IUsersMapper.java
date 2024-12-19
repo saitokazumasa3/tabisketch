09110179cf427eebf3caa19ce136be82ede84dcc
imanoari <101164506+imanoari@users.noreply.github.com>
2024-12-19 10:12:43 +0900

    Mapperの編集内容を削除

--- src/main/java/com/tabisketch/mapper/IUsersMapper.java
+++ src/main/java/com/tabisketch/mapper/IUsersMapper.java
@@ -20,7 +20,4 @@ public interface IUsersMapper {
 
     @Select("SELECT COUNT(*) FROM users WHERE mail_address = #{mailAddress}")
     int isExistMailAddress(final String mailAddress);
-
-    @Update("UPDATE users SET password = #{password} WHERE mail_address = #{mailAddress}")
-    int updatePassword(final String mailAddress, final String password);
 }
